9c0899114e69d0d53c9ecfb4c80810e6956429ce
wanglekai <xiaoguailele@foxmail.com>
2021-04-27 22:07:53 +0800

    recommend/banner

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>recommend</div>
+  <div class="slider-wrapper">
+    <div class="slider-content">
+      <slider v-if="sliders.length" :sliders="sliders"></slider>
+    </div>
+  </div>
 </template>
 <script>
+import Slider from '../components/base/slider'
 import { getRecommend } from './../service/recommend'
 export default {
   name: 'recommend',
+  components: {
+    Slider
+  },
+  data() {
+      return {
+        sliders: []
+      }
+  },
   async created () {
     const result = await getRecommend()
-    console.log(result)
+    // console.log(result)
+    this.sliders = result.sliders
   }
 }
 </script>
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+  .slider-wrapper {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-top: 40%;
+    overflow: hidden;
+    .slider-content {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
 </style>
