5b92bd4c5263f89f5699816f7fea45897f66a824
wanglekai <xiaoguailele@foxmail.com>
2021-04-28 00:17:08 +0800

    add recommend-list

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,43 +1,124 @@
 <template>
-  <div class="slider-wrapper">
-    <div class="slider-content">
-      <slider v-if="sliders.length" :sliders="sliders"></slider>
+<div class="recommend">
+  <scroll class="recommend-content">
+    <div>
+      <div class="slider-wrapper">
+        <div class="slider-content">
+          <slider v-if="sliders.length" :sliders="sliders"></slider>
+        </div>
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul>
+          <li
+            v-for="item in albums"
+            class="item"
+            :key="item.id"
+            @click="selectItem(item)"
+          >
+            <div class="icon">
+              <img width="60" height="60" v-lazy="item.pic" />
+            </div>
+            <div class="text">
+              <h2 class="name">
+                {{ item.username }}
+              </h2>
+              <p class="title">
+                {{ item.title }}
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
+  </scroll>
+</div>
 </template>
 <script>
 import Slider from '../components/base/slider'
+import Scroll from '../components/base/scroll'
 import { getRecommend } from './../service/recommend'
 export default {
   name: 'recommend',
   components: {
-    Slider
+    Slider,
+    Scroll
   },
   data() {
-      return {
-        sliders: []
-      }
+    return {
+      sliders: [],
+      albums: []
+    }
   },
-  async created () {
+  async created() {
     const result = await getRecommend()
     // console.log(result)
     this.sliders = result.sliders
+    this.albums = result.albums
   }
 }
 </script>
 <style lang="scss" scoped>
-  .slider-wrapper {
-    position: relative;
+  .recommend {
+    position: fixed;
     width: 100%;
-    height: 0;
-    padding-top: 40%;
-    overflow: hidden;
-    .slider-content {
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: 100%;
+    top: 44px;
+    bottom: 0;
+    overflow: scroll;
+    .recommend-content {
       height: 100%;
+      overflow: hidden;
+      .slider-wrapper {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-top: 40%;
+        overflow: hidden;
+        .slider-content {
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+        }
+      }
+      .recommend-list {
+        .list-title {
+          height: 65px;
+          line-height: 65px;
+          text-align: center;
+          font-size: $font-size-medium;
+          color: $color-theme;
+        }
+        .item {
+          display: flex;
+          box-sizing: border-box;
+          align-items: center;
+          padding: 0 20px 20px 20px;
+
+          .icon {
+            flex: 0 0 60px;
+            width: 60px;
+            padding-right: 20px;
+          }
+          .text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1;
+            line-height: 20px;
+            overflow: hidden;
+            font-size: $font-size-medium;
+          }
+          .name {
+            margin-bottom: 10px;
+            color: $color-text;
+          }
+          .title {
+            color: $color-text-d;
+          }
+        }
+      }
     }
   }
 </style>
